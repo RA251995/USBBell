06457a5120d6fe6df9b5e684e4fbd9600d4443ef
RA <rijoabraham251995@gmail.com>
2017-04-11 10:50:41 +0530

    Add files via upload

---
+++ Rename.vb
@@ -0,0 +1,18 @@
+﻿Imports MaterialSkin
+Public Class Rename
+
+    Private Sub bOK_Click(sender As Object, e As EventArgs)
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+    End Sub
+
+    Private Sub Rename_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeMaterialSkin()
+    End Sub
+    Private Sub InitializeMaterialSkin()
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.DARK
+        SkinManager.ColorScheme = New ColorScheme(Primary.Grey800, Primary.Grey900, Primary.Grey500, Accent.Amber100, TextShade.WHITE)
+    End Sub
+
+End Class
